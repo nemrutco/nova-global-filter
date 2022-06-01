1c72d10b9a0445095fd789bf768eb976aa01e14b
digislexia <31546752+digislexia@users.noreply.github.com>
2022-06-01 16:54:50 +0300

    allow to use custom key() and filters with required constructor arguments

--- src/GlobalFilterable.php
+++ src/GlobalFilterable.php
@@ -16,22 +16,31 @@ trait GlobalFilterable
 
             $request->range = optional($request)->range ?? 3600;
 
+            $filtersByKeys = [];
+            $filtersKeys = [];
+            foreach ($filters as $filter) {
+                $currentFilter = gettype($filter) === 'string' ? new $filter : $filter;
+                $key = $currentFilter->key() ?? $currentFilter::class;
+                $filtersByKeys[$key] = $currentFilter;
+                $filtersKeys[] = $key;
+            }
             foreach (json_decode($request->filters, true) as $filter => $value) {
-                if (empty($value) || !in_array($filter, $filters)) {
+                if (empty($value) || !in_array($filter, $filtersKeys)) {
                     continue;
                 }
 
-                $model = (new $filter)->apply($request, $model, $value);
+                $model = $filtersByKeys[$filter]->apply($request, $model, $value);
             }
         }else {
             foreach ($filters as $filter) {
-                $currentFilter = new $filter;
-                
+                $currentFilter = gettype($filter) === 'string' ? new $filter : $filter;
+
                 if(!empty($currentFilter->default())) {
                     $model = $currentFilter->apply($request, $model, $currentFilter->default());
                 }
             }
         }
+
         return $model;
     }
 }
