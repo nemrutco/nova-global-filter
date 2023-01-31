d5e5b9277df49af8e52cb6b1a043d255aa3296fa
Davide Cattani <davide.cattani@studioilgranello.it>
2023-01-31 09:47:45 +0100

    fix indents

--- src/GlobalFilterable.php
+++ src/GlobalFilterable.php
@@ -6,46 +6,46 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait GlobalFilterable
 {
-  public function globalFiltered($model, $filters = [])
-  {
-    $request = request();
+    public function globalFiltered($model, $filters = [])
+    {
+        $request = request();
 
-    $model = $model instanceof Builder ? $model : (new $model)->newQuery();
+        $model = $model instanceof Builder ? $model : (new $model)->newQuery();
 
-    // manage filters changed on UI
-    if ($request->has('filters')) {
+        // manage filters changed on UI
+        if ($request->has('filters')) {
 
-      $request->range = optional($request)->range ?? 3600;
+            $request->range = optional($request)->range ?? 3600;
 
-      $requestFilters = array_keys(json_decode($request->filters, true));
+            $requestFilters = array_keys(json_decode($request->filters, true));
 
-      foreach (json_decode($request->filters, true) as $filter => $value) {
-        if (empty($value) || !in_array($filter, $filters)) {
-          continue;
+            foreach (json_decode($request->filters, true) as $filter => $value) {
+                if (empty($value) || !in_array($filter, $filters)) {
+                    continue;
+                }
+
+                $model = (new $filter)->apply($request, $model, $value);
+            }
+
+            // need to apply default filters if they are not changed on UI
+            foreach ($filters as $filter) {
+                $currentFilter = new $filter;
+
+                if (!empty($currentFilter->default()) && !in_array(get_class($currentFilter), $requestFilters)) {
+                    $model = $currentFilter->apply($request, $model, $currentFilter->default());
+                }
+            }
+
+        } // manage default filter values (no filter changed on UI still)
+        else {
+            foreach ($filters as $filter) {
+                $currentFilter = new $filter;
+
+                if (!empty($currentFilter->default())) {
+                    $model = $currentFilter->apply($request, $model, $currentFilter->default());
+                }
+            }
         }
-
-        $model = (new $filter)->apply($request, $model, $value);
-      }
-
-      // need to apply default filters if they are not changed on UI
-      foreach ($filters as $filter) {
-        $currentFilter = new $filter;
-
-        if (!empty($currentFilter->default()) && !in_array(get_class($currentFilter), $requestFilters)) {
-          $model = $currentFilter->apply($request, $model, $currentFilter->default());
-        }
-      }
-
-    } // manage default filter values (no filter changed on UI still)
-    else {
-      foreach ($filters as $filter) {
-        $currentFilter = new $filter;
-
-        if (!empty($currentFilter->default())) {
-          $model = $currentFilter->apply($request, $model, $currentFilter->default());
-        }
-      }
+        return $model;
     }
-    return $model;
-  }
 }
